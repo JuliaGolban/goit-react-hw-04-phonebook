f1c0228631618bdbebc7a63b213b8471062244d2
JuliaGolban <julia.j.shcherban@gmail.com>
2022-11-10 14:01:04 +0200

    refactor form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,69 +1,138 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import TextBtn from 'components/buttons/TextBtn/TextBtn.styled';
-import { Form, Field, Label, Input} from './Form.styled';
+import { Form, Field, Label, Input } from './Form.styled';
 
-const INITIAL_STATE = {
-    name: '',
-    number:'',
-}
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [id, setId] = useState(null);
 
-export class ContactForm extends Component {
-    state = { ...INITIAL_STATE};
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    const contactId = nanoid(4);
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
 
-    handleChange = e => {
-        const { name,  value} = e.currentTarget;
-        this.setState({ [name]: value})
-        this.createID();
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
     }
+    setId(contactId);
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.onSubmit({ ...this.state });
-        this.reset();
-    }
-        
-    createID = () => {
-        const contactId = nanoid(4);
-        this.setState({ id: contactId })
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number, id });
+    reset();
+  };
 
-    reset = () => {
-        this.setState({ ...INITIAL_STATE });
-    };
-    
-    render() {
-        const { id, name, number } = this.state;
+  const reset = () => {
+    setName('');
+    setNumber('');
+    setId(null);
+  };
 
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <Field>
-                    <Label>Name</Label>
-                    <Input
-                        type="text"
-                        name="name"
-                        contactId={id}
-                        value={name}
-                        onChange={this.handleChange}
-                        pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required />
-                </Field>
-                <Field>
-                    <Label>Number</Label>
-                    <Input
-                        type="tel"
-                        name="number"
-                        contactId={id}
-                        value={number}
-                        onChange={this.handleChange}
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                     />
-                </Field>
-                <TextBtn type="submit">Add contact</TextBtn>
-            </Form>
-        );
-    }
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Field>
+        <Label>Name</Label>
+        <Input
+          type="text"
+          name="name"
+          contactId={id}
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </Field>
+      <Field>
+        <Label>Number</Label>
+        <Input
+          type="tel"
+          name="number"
+          contactId={id}
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </Field>
+      <TextBtn type="submit">Add contact</TextBtn>
+    </Form>
+  );
+};
+
+// const INITIAL_STATE = {
+//   name: '',
+//   number: '',
+// };
+
+// export class ContactForm extends Component {
+//   state = { ...INITIAL_STATE };
+
+//   handleChange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({ [name]: value });
+//     this.createID();
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   createID = () => {
+//     const contactId = nanoid(4);
+//     this.setState({ id: contactId });
+//   };
+
+//   reset = () => {
+//     this.setState({ ...INITIAL_STATE });
+//   };
+
+//   render() {
+//     const { id, name, number } = this.state;
+
+//     return (
+//       <Form onSubmit={this.handleSubmit}>
+//         <Field>
+//           <Label>Name</Label>
+//           <Input
+//             type="text"
+//             name="name"
+//             contactId={id}
+//             value={name}
+//             onChange={this.handleChange}
+//             pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//           />
+//         </Field>
+//         <Field>
+//           <Label>Number</Label>
+//           <Input
+//             type="tel"
+//             name="number"
+//             contactId={id}
+//             value={number}
+//             onChange={this.handleChange}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//           />
+//         </Field>
+//         <TextBtn type="submit">Add contact</TextBtn>
+//       </Form>
+//     );
+//   }
+// }
